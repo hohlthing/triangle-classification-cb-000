bf23147d3d91ff0872ccd9ae9dc3bb2b8ed01593
michael hohl <mhohl@rocketmail.com>
2018-03-26 19:32:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize (a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if !valid?
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a ==c
+      :isosceles
+    elsif a != b && b != c && a !=c
+      :scalene
+    end
+
+  def valid?
+    if a + b <= c || b + c <= a || a + c <= b || a <= 0 || b <= 0 || c <= 0
+      false
+    else
+      true
+    end
+end
+
+class TriangleError < StandardError
 end
